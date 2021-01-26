8de44250bbdd6fdad38ac03ef6f0626ce25a50ee
GaM8BiT <amiran.bo@gmail.com>
2021-01-26 22:54:01 +0300

    v2

--- client/src/components/gg/Slider.jsx
+++ client/src/components/gg/Slider.jsx
@@ -1,32 +1,30 @@
 import React from 'react'
-import classes from "./main.module.css"
-import slideShow from "./main.js"
+import classes from "./slider.module.css"
+
 const Slider = () => {
-  slideShow(classes.slider,{isAutoplay: true})
+  
   return (
 
-  <div className={classes.slider}>
-    <div className={classes.slider__wrapper}>
-      <div className={classes.slider__items}>
-        <div className={classes.slider__item}>
-          <div style={{height: "250px", background: "orange"}}></div>
-        </div>
-        <div className={classes.slider__item}>
-          <div style={{height: "250px", background: "green"}}></div>
-        </div>
-        <div className={classes.slider__item}>
-          <div style={{height: "250px", background: "violet"}}></div>
-        </div>
-        <div className={classes.slider__item}>
-          <div style={{height: "250px", background: "coral"}}></div>
-        </div>
-      </div>
+    <div className={classes.wrapper}>
+    <input type="radio" name="point" id="slide1" checked/>
+    <input type="radio" name="point" id="slide2"/>
+    <input type="radio" name="point" id="slide3"/>
+    <input type="radio" name="point" id="slide4"/>
+    <input type="radio" name="point" id="slide5"/>
+    <div className={classes.slider}>
+      <div class="slides slide1"></div>
+      <div class="slides slide2"></div>
+      <div class="slides slide3"></div>
+      <div class="slides slide4"></div>
+      <div class="slides slide5"></div>
+    </div>	
+    <div className={classes.controls}>
+      <label for="slide1"></label>
+      <label for="slide2"></label>
+      <label for="slide3"></label>
+      <label for="slide4"></label>
+      <label for="slide5"></label>
     </div>
-    <div>
-    <a className={classes.slider__control + ' ' + classes.slider__control_prev} href="#" role="button"></a>
-    <a className={classes.slider__control + ' ' + classes.slider__control_next + ' ' + classes.slider__control_show} href="#" role="button"></a>
-    </div>
-    
   </div>
   
   )
