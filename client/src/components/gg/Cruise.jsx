f662b9ffced3dbe2bfd8c235935d96e9b478ce40
GaM8BiT <amiran.bo@gmail.com>
2021-02-02 21:25:11 +0300

    Tickets v2
    to be continued

--- client/src/components/gg/Cruise.jsx
+++ client/src/components/gg/Cruise.jsx
@@ -13,15 +13,40 @@ function App() {
                                 <input type="checkbox" className={classes.input}/>
                                 <span className={classes.checker}>
                                     <p>Все</p>
+                                </span>
+                            </label>
+                            <label>
+                                <input type="checkbox" className={classes.input}/>
+                                <span className={classes.checker}>
                                     <p>Без пересадок</p>
+                                </span>
+                            </label>
+                            <label>
+                                <input type="checkbox" className={classes.input}/>
+                                <span className={classes.checker}>
                                     <p>1 пересадка</p>
+                                </span>
+                            </label>
+                            <label>
+                                <input type="checkbox" className={classes.input}/>
+                                <span className={classes.checker}>
                                     <p>2 пересадки</p>
+                                </span>
+                            </label>
+                            <label>
+                                <input type="checkbox" className={classes.input}/>
+                                <span className={classes.checker}>
                                     <p>3 пересадки</p>
                                 </span>
                             </label>
                         </form>
                     </div>
-                    <div className={classes.filter}>2 слот</div>
+                    <div className={classes.filter}>
+
+                    <div className={classes.filter_low_Price + ' ' + classes.filter_element}>Самый дешевый</div>
+                    <div className={classes.filter_faster_Price + ' ' + classes.filter_element}>Самый быстрый</div>
+
+                    </div>
                     <div className={classes.tickets}>3 слот</div>
                 </div>
             </div>
